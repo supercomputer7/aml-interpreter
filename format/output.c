6f64f65b1a1340965714c3c2e0a93af856465de1
Liav A <liavalb@gmail.com>
2020-03-12 10:58:01 +0200

    Output: Add more text to print_help() and print_version()

--- format/output.c
+++ format/output.c
@@ -16,9 +16,17 @@ void set_output_format(FormatOutput* inital_format_output)
 
 void print_help()
 {
+    print_version();
+    printf("usage: amldecode [options] [aml_binary]\n");
+    printf("\n 'aml_binary' is a raw binary file contains DSDT & AML bytecode\n");
+
+    printf("\nStandard options:\n");
+    printf("-h or --help\tdisplay this help and exit\n");
+    printf("-version\tdisplay version information and exit\n");
 }
 
 void print_version()
 {
+    printf("Userspace AML Interpreter version 0.1\n");
     printf("Copyright (c) 2020 Liav A.\n");
 }
